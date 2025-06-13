fb425e048b7ae9a52303a26ea5f7f8069ca57193
Oct <rcabrera@outlook.com>
2025-06-13 14:21:54 -0400

    Added CI/CD workflow

--- vitest.config.js
+++ vitest.config.js
@@ -3,5 +3,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
+    reporters: process.env.GITHUB_ACTIONS ? ['verbose', 'github-actions','junit','json'] : ['verbose'],
+    outputFile: {
+      junit: './junit-report.xml',
+      json: './json-report.json',
+    },
+    coverage: {
+      provider: 'v8'
+    },
   },
 });
